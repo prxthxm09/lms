9fd2ed2a17530e56a4924e5713ca384d518535cf
venom <110739268+prxthxm09@users.noreply.github.com>
2023-02-17 00:35:12 +0530

    updated

---
+++ lms/conn.php
@@ -0,0 +1,7 @@
+<?php
+define("DBHOST","sql207.epizy.com");
+define("DBUSERNAME","epiz_31245415");
+define("DBPASSWORD","wvPUWKdxQeOdGla");
+define("DB","epiz_31245415_lib");
+$conn = mysqli_connect(DBHOST,DBUSERNAME,DBPASSWORD,DB);
+?>
